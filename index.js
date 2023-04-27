a45b74c4ca11541d12082517d56e3ac31015ae3e
Anna Cole <acrrj123@gmail.com>
2023-04-27 17:11:49 -0500

    array.find method

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+//write a function superbowlWin()
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(array) {
+    const result = array.find(array => array.result === 'W')
+        return result ? result.year : undefined
+}
+console.log(superbowlWin(record))
+
+
+
+
+
+
+
+
+
+
